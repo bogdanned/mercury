971d9abc3e12502612324077279d17471b965bc4
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-10-05 17:12:18 +0200

    first model

--- data_structure_creation.py
+++ data_structure_creation.py
@@ -6,7 +6,7 @@ import numpy as np
 import tflearn
 import tensorflow as tf
 import random
-
+import pickle
 
 # import our chat-bot intents file
 import json
@@ -74,3 +74,22 @@ training = np.array(training)
 # create train and test lists
 train_x = list(training[:,0])
 train_y = list(training[:,1])
+
+
+#reset underlying graph data
+tf.reset_default_graph()
+# Build neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+# Start training (apply gradient descent algorithm)
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
+
+# save all of our data structures
+pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
